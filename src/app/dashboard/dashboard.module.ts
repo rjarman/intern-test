887e7c771362a4860e7de6620ccb359033a9493a
Rafsun Jany Arman <armanrafsunjany@gmail.com>
2022-01-19 14:46:47 +0600

    refactor: add necessary modules

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,52 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CreateComponent } from './create/create.component';
 import { TableComponent } from './table/table.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AboutComponent } from './about/about.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MatRippleModule,
+  MAT_RIPPLE_GLOBAL_OPTIONS,
+} from '@angular/material/core';
+import { DetailsComponent } from './details/details.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
-  imports: [CommonModule, DashboardRoutingModule],
-  declarations: [CreateComponent, TableComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatRippleModule,
+  ],
+  declarations: [
+    CreateComponent,
+    TableComponent,
+    AboutComponent,
+    DetailsComponent,
+    UpdateComponent,
+  ],
+  providers: [
+    {
+      provide: MAT_RIPPLE_GLOBAL_OPTIONS,
+      useValue: {
+        animation: {
+          enterDuration: 250,
+          exitDuration: 50,
+        },
+      },
+    },
+  ],
 })
 export class DashboardModule {}
